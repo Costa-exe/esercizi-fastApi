2bc4d0b6d045cd47b54571230da0c7b770685c40
Fabio Cosentino <fabiocosentino7@gmail.com>
2022-12-05 14:58:12 +0100

    commit finale

--- dto/fileGenerator.py
+++ dto/fileGenerator.py
@@ -1,7 +1,7 @@
 class FileGen:
     @classmethod
     def textFile(cls, nomeFile, contenuto):
-        with open(f"{nomeFile}.txt", 'w') as f:
+        with open(f"{nomeFile}.txt", 'wb') as f:
             f.write(contenuto)
         
         return f"{nomeFile}.txt"
--- main.py
+++ main.py
@@ -4,5 +4,7 @@ from dataServices.service import Services
 app = FastAPI()
 
 @app.get("/zip")
-async def zipBytes(stringa : str): 
-   return Services.zip(Services.FileGenerate('fileDiTesto', Services.bytes(stringa)))
+async def zipBytes(stringa : str):
+    with open(Services.zip(Services.FileGenerate('fileDiTesto', Services.bytes(stringa))), 'rb') as file_data:
+        bytes_content = file_data.read()
+        return bytes_content.hex()
